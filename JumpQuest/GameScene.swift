453901ae96514e3507f62c07458a0ff7848543a6
Kalvin Loc <kalvinloc@kalvins-macbook-pro.local>
2015-07-01 18:48:06 -0700

    - Added Camera to pan map by direction keys and zoom in out with ' / ] keys

--- JumpQuest/GameScene.swift
+++ JumpQuest/GameScene.swift
@@ -20,7 +20,9 @@ class GameScene: SKScene {
             
             for e in mapElements {
                 if e is NSDictionary{
-                    if e.valueForKey("-name") as! String == "5" {
+                    let section = e.valueForKey("-name") as! String
+                    
+                    if section == "5" || section == "1" || section == "2" || section == "3"  {
                         var cords = e.valueForKey("imgdir") as! NSArray
                         cords = cords[1].valueForKey("imgdir") as! NSArray
                         
@@ -29,13 +31,16 @@ class GameScene: SKScene {
                                 let intVals = n.valueForKey("int") as! NSArray
                                 let x = Int(intVals[0].valueForKey("-value") as! String)
                                 let y = Int(intVals[1].valueForKey("-value") as! String)
+                                let zM = Int(intVals[3].valueForKey("-value") as! String)
                                 let no = intVals[2].valueForKey("-value") as! String
                                 let u = n.valueForKey("string")!.valueForKey("-value") as! String
                                 
                                 let spriteTextureName:String = u + "." + no
                                 
                                 let sprite = SKSpriteNode(imageNamed: spriteTextureName)
+                                //sprite.anchorPoint = CGPointMake(0.6, -0.7)
                                 sprite.position = CGPointMake(CGFloat(x!), CGFloat(y!))
+                                sprite.zPosition = CGFloat(zM!)
                                 sprite.setScale(1)
                                 
                                 self.addChild(sprite)
@@ -54,13 +59,51 @@ class GameScene: SKScene {
         
         let location = theEvent.locationInNode(self)
         
-        let sprite = SKSpriteNode(imageNamed:"edU.0")
+        let sprite = SKSpriteNode(imageNamed:"edD.0")
+        sprite.anchorPoint = CGPointMake(0.0, 1.0)
         sprite.position = location;
         
-        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-        sprite.runAction(SKAction.repeatActionForever(action))
-        
         self.addChild(sprite)
+    }
+    
+    override func keyDown(theEvent: NSEvent) {
+        super.keyDown(theEvent)
+        
+        let s   =   theEvent.charactersIgnoringModifiers!
+        let s1  =   s.unicodeScalars
+        let s2  =   s1[s1.startIndex].value
+        let s3  =   Int(s2)
+        
+        switch s3 {
+        case NSUpArrowFunctionKey:
+            camera?.runAction(SKAction(named: "CMoveUp")!)
+            return
+            
+        case NSDownArrowFunctionKey:
+            camera?.runAction(SKAction(named: "CMoveDown")!)
+            return
+            
+        case NSRightArrowFunctionKey:
+            camera?.runAction(SKAction(named: "CMoveRight")!)
+            return
+            
+        case NSLeftArrowFunctionKey:
+            camera?.runAction(SKAction(named: "CMoveLeft")!)
+            return
+            
+        case 39: // ]
+            camera?.xScale += 0.1
+            camera?.yScale += 0.1
+            return
+            
+        case 93: // '
+            camera?.xScale -= 0.1
+            camera?.yScale -= 0.1
+            return
+            
+        default:
+            return
+        }
     }
     
     override func update(currentTime: CFTimeInterval) {
